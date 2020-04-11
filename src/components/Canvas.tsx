9c260ed7ee67ef48340bb10bf9c6b78bb7c92f02
Suh Donghwi <hwidongsuh@gmail.com>
2020-04-12 03:59:26 +0900

    feat: add drawAxis function

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,13 +1,25 @@
 import React, {useRef, useEffect} from 'react';
 
+function drawAxis(ctx: CanvasRenderingContext2D, width: number, height: number) {
+  ctx.moveTo(0, height / 2);
+  ctx.lineTo(width, height / 2);
+  ctx.stroke();
+
+  ctx.moveTo(width / 2, 0);
+  ctx.lineTo(width / 2, height);
+  ctx.stroke();
+}
+
 export default function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasWidth = 1000;
+  const canvasHeight = 500;
 
   useEffect(() => {
     const ctx = canvasRef.current!.getContext('2d')!;
-    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-    ctx.fillRect(30, 30, 50, 50);
+
+    drawAxis(ctx, canvasWidth, canvasHeight);
   }, [canvasRef]);
 
-  return <canvas ref={canvasRef} />;
+  return <canvas ref={canvasRef} height={canvasHeight} width={canvasWidth} />;
 }
