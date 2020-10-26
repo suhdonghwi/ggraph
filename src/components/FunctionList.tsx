d4caf2adb73f8c022ff3217579f586e10775f066
Suh Donghwi <hwidongsuh@gmail.com>
2020-10-26 16:30:35 +0900

    feat: add styling to function list item

--- src/components/FunctionList.tsx
+++ src/components/FunctionList.tsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import styled from "styled-components/macro";
 
 const Container = styled.div`
-  padding: 2rem;
+  padding: 1.2rem 2rem;
   background-color: white;
   box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  border-radius: 10px;
   position: absolute;
   top: 2rem;
   left: 2rem;
@@ -13,13 +14,23 @@ const Container = styled.div`
 
 const List = styled.ul`
   padding: 0;
+  list-style-type: none;
 `;
 
 const ListItem = styled.li`
-   ;
+  font-family: monospace;
+  font-size: 1.2rem;
+  border-radius: 10px;
+  border: 1px solid #dee2e6;
+  padding: 0.5rem 1rem;
+
+  margin-bottom: 0.5rem;
 `;
 
-const Input = styled.input``;
+const Input = styled.input`
+  font-family: monospace;
+  font-size: 1.2rem;
+`;
 
 interface FunctionListProps {
   value: string[];
